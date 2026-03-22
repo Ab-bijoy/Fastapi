24a4a90d2dfd5bd09831a57df9509bc0f2675f84
Anindo Barua <abbijoy2004040@gmail.com>
2026-03-22 16:51:36 +0600

    todo app

---
+++ Project_1/src/project/static/js/config.js
@@ -0,0 +1,8 @@
+// ============================================
+// Frontend Configuration
+// ============================================
+// Change this URL when switching environments.
+
+const CONFIG = {
+    API_BASE: 'https://scgaehwhzzuslpktk4spdbfiga0xmtnu.lambda-url.ap-southeast-1.on.aws',
+};
